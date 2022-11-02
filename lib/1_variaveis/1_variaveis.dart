287c87af5b80d2c6e1698f9254d41240009d6b53
Renan Alencar <renanalencar02@gmail.com>
2022-11-01 21:26:47 -0300

    Alterado arquivo variaveis

--- lib/1_variaveis/1_variaveis.dart
+++ lib/1_variaveis/1_variaveis.dart
@@ -1,10 +1,29 @@
-void main(){
+void main() {
 
+  //pai do int e double, todos herdam dele, aceita qualquer valor
+  num qualquerNumero;
+ 
+  //inteiros
   int idade = 29;
+  //reais
   double valorIphone = 12.000;
+  //textos
   String nomeCurso = 'Academia do Flutter!';
-
   
+  //para variáveis que já tem um valor definido, no caso 50 é um inteiro, não precisa tipar ela como int, apenas com var.
+  var valorDefinido = 50;
+
+  //padrão de criação de variáveis: camelCase
+  String nomeMelhorCursoFlutter;
+
+  //todas as classes do sistema herdam de Object, todo mundo é filho de Object
+  Object todasClasse;
+
+  //tipo dinamico, pode armazenar qualquer coisa
+  dynamic dynamicQualquer;
+
+  //evitar usar Object e dynamic
+
 
 
 }
